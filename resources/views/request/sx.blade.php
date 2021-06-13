97fbd70c5b995002c3e4931686a2c16292309ec1
Kåre Slettnes <kaare.slettnes@tffk.no>
2021-06-13 22:14:02 +0200

    Issue #2: Fix/remove NS prefix in requests

--- resources/views/request/sx.blade.php
+++ resources/views/request/sx.blade.php
@@ -1,30 +1,24 @@
 <?xml version="1.0" encoding="utf-8"?>
-<AE:Siri version="1.4"
-         xmlns:AB="http://www.ifopt.org.uk/ifopt"
-         xmlns:AC="http://datex2.eu/schema/1_0/1_0"
-         xmlns:AD="http://www.ifopt.org.uk/acsb"
-         xmlns:AE="http://www.siri.org.uk/siri"
-         xmlns:xs="http://www.w3.org/2001/XMLSchemainstance">
-  <AE:SubscriptionRequest>
-    <AE:RequestTimestamp>{{ $request_date }}</AE:RequestTimestamp>
-    <AE:RequestorRef>{{ $subscription->requestor_ref }}</AE:RequestorRef>
-    <AE:MessageIdentifier>{{ $message_identifier }}</AE:MessageIdentifier>
-    <AE:ConsumerAddress>{{ $consumer_address }}</AE:ConsumerAddress>
+<Siri version="1.4" xmlns="http://www.siri.org.uk/siri">
+  <SubscriptionRequest>
+    <RequestTimestamp>{{ $request_date }}</RequestTimestamp>
+    <RequestorRef>{{ $subscription->requestor_ref }}</RequestorRef>
+    <ConsumerAddress>{{ $consumer_address }}</ConsumerAddress>
 
-    <AE:SubscriptionContext>
-      <AE:HeartbeatInterval>PT{{ $subscription->heartbeat_interval }}M</AE:HeartbeatInterval>
-    </AE:SubscriptionContext>
+    <SubscriptionContext>
+      <HeartbeatInterval>{{ $subscription->heartbeat_interval }}</HeartbeatInterval>
+    </SubscriptionContext>
 
-    <AE:SituationExchangeSubscriptionRequest>
-      <AE:SubscriberRef>{{ $subscription->requestor_ref }}</AE:SubscriberRef>
-      <AE:SubscriptionIdentifier>{{ $subscription->id }}</AE:SubscriptionIdentifier>
-      <AE:InitialTerminationTime>{{ $subscription_ttl }}</AE:InitialTerminationTime>
+    <SituationExchangeSubscriptionRequest>
+      <SubscriberRef>{{ $subscription->requestor_ref }}</SubscriberRef>
+      <SubscriptionIdentifier>{{ $subscription->id }}</SubscriptionIdentifier>
+      <InitialTerminationTime>{{ $subscription_ttl }}</InitialTerminationTime>
 
-      <AE:SituationExchangeRequest version="1.4">
-        <AE:RequestTimestamp>{{ $request_date }}</AE:RequestTimestamp>
-        <AE:PreviewInterval>{{ $preview_interval }}</AE:PreviewInterval>
-      </AE:SituationExchangeRequest>
+      <SituationExchangeRequest version="1.4">
+        <RequestTimestamp>{{ $request_date }}</RequestTimestamp>
+        <PreviewInterval>{{ $preview_interval }}</PreviewInterval>
+      </SituationExchangeRequest>
 
-    </AE:SituationExchangeSubscriptionRequest>
-  </AE:SubscriptionRequest>
-</AE:Siri>
+    </SituationExchangeSubscriptionRequest>
+  </SubscriptionRequest>
+</Siri>
--- resources/views/request/vm.blade.php
+++ resources/views/request/vm.blade.php
@@ -1,29 +1,23 @@
 <?xml version="1.0" encoding="utf-8"?>
-<AE:Siri version="1.4"
-         xmlns:AB="http://www.ifopt.org.uk/ifopt"
-         xmlns:AC="http://datex2.eu/schema/1_0/1_0"
-         xmlns:AD="http://www.ifopt.org.uk/acsb"
-         xmlns:AE="http://www.siri.org.uk/siri"
-         xmlns:xs="http://www.w3.org/2001/XMLSchemainstance">
-  <AE:SubscriptionRequest>
-    <AE:RequestTimestamp>{{ $request_date }}</AE:RequestTimestamp>
-    <AE:RequestorRef>{{ $subscription->requestor_ref }}</AE:RequestorRef>
-    <AE:MessageIdentifier>{{ $message_identifier }}</AE:MessageIdentifier>
-    <AE:ConsumerAddress>{{ $consumer_address }}</AE:ConsumerAddress>
+<Siri version="1.4" xmlns="http://www.siri.org.uk/siri">
+  <SubscriptionRequest>
+    <RequestTimestamp>{{ $request_date }}</RequestTimestamp>
+    <RequestorRef>{{ $subscription->requestor_ref }}</RequestorRef>
+    <ConsumerAddress>{{ $consumer_address }}</ConsumerAddress>
 
-    <AE:SubscriptionContext>
-      <AE:HeartbeatInterval>{{ $subscription->heartbeat_interval }}</AE:HeartbeatInterval>
-    </AE:SubscriptionContext>
+    <SubscriptionContext>
+      <HeartbeatInterval>{{ $subscription->heartbeat_interval }}</HeartbeatInterval>
+    </SubscriptionContext>
 
-    <AE:VehicleMonitoringSubscriptionRequest>
-      <AE:SubscriberRef>{{ $subscription->requestor_ref }}</AE:SubscriberRef>
-      <AE:SubscriptionIdentifier>{{ $subscription->id }}</AE:SubscriptionIdentifier>
-      <AE:InitialTerminationTime>{{ $subscription_ttl }}</AE:InitialTerminationTime>
+    <VehicleMonitoringSubscriptionRequest>
+      <SubscriberRef>{{ $subscription->requestor_ref }}</SubscriberRef>
+      <SubscriptionIdentifier>{{ $subscription->id }}</SubscriptionIdentifier>
+      <InitialTerminationTime>{{ $subscription_ttl }}</InitialTerminationTime>
 
-      <AE:VehicleMonitoringRequest version="1.4">
-        <AE:RequestTimestamp>{{ $request_date }}</AE:RequestTimestamp>
-      </AE:VehicleMonitoringRequest>
+      <VehicleMonitoringRequest version="1.4">
+        <RequestTimestamp>{{ $request_date }}</RequestTimestamp>
+      </VehicleMonitoringRequest>
 
-    </AE:VehicleMonitoringSubscriptionRequest>
-  </AE:SubscriptionRequest>
-</AE:Siri>
+    </VehicleMonitoringSubscriptionRequest>
+  </SubscriptionRequest>
+</Siri>
