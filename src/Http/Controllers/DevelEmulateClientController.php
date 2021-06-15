4eab323ddd245824ddb28f26baad0eb328aefc14
Kåre Slettnes <kaare.slettnes@tffk.no>
2021-06-15 15:13:40 +0200

    Attempting to get devel client emulation page in place

---
+++ src/Http/Controllers/DevelEmulateClientController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace TromsFylkestrafikk\Siri\Http\Controllers;
+
+use TromsFylkestrafikk\Siri\Models\SiriSubscription;
+use Illuminate\Http\Request;
+
+class DevelEmulateClientController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('devel.upload-xml', ['subscriptions' => $this->getSubscriptions()]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $channel = $request->input('siri_channel');
+        switch ($channel) {
+            case 'ET':
+                $this->queuedHandling = true;
+                break;
+        }
+        // $result = $this->processorGate(fopen($request->file('siri-xml')->path(), 'r'));
+        return view('devel.upload-xml', $this->getSubscriptions());
+    }
+
+    protected function getSubscriptions()
+    {
+        return SiriSubscription::all(['id', 'channel', 'subscription_url']);
+    }
+}
