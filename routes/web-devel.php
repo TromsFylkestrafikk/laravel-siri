8c9e058e20ee0ff7d42ae3674c120222cbf5dae3
Kåre Slettnes <kaare.slettnes@tffk.no>
2021-06-16 09:35:03 +0200

    Issue #3: Make emulated client a resource controller

--- routes/web-devel.php
+++ routes/web-devel.php
@@ -7,5 +7,4 @@
 use Illuminate\Support\Facades\Route;
 use TromsFylkestrafikk\Siri\Http\Controllers\DevelEmulateClientController;
 
-Route::get('devel/consume', [DevelEmulateClientController::class, 'uploadXml'])->name('siri.devel.consume.form');
-Route::post('devel/consume', [DevelEmulateClientController::class, 'submitXml'])->name('siri.devel.consume.post');
+Route::resource('devel/emulate', DevelEmulateClientController::class)->only(['create', 'store']);
