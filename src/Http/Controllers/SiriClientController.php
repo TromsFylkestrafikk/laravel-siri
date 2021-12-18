761fdacd51b547e1eab2ae79c5bf5d04cec555fb
Kåre Slettnes <kaare.slettnes@tffk.no>
2021-12-18 15:52:41 +0100

    Issue #3: Cleaned up duplicate repositories entry in composer.json

--- src/Http/Controllers/SiriClientController.php
+++ src/Http/Controllers/SiriClientController.php
@@ -2,12 +2,11 @@
 
 namespace TromsFylkestrafikk\Siri\Http\Controllers;
 
-use Illuminate\Http\Request;
 use TromsFylkestrafikk\Siri\Models\SiriSubscription;
 
 class SiriClientController extends Controller
 {
-    public function consume(Request $request, $channel, SiriSubscription $subscription)
+    public function consume($channel, SiriSubscription $subscription)
     {
         return sprintf("Got request of type %s on subscription '%s'", $channel, $subscription->id);
     }
