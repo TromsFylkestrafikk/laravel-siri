3da9deff7ea4d468dff049bc5081b0548d72dbff
Kåre Slettnes <kaare@slettnes.org>
2022-01-07 15:24:34 +0100

    Issue #8: Stub for subscription response callback

--- src/Http/Controllers/SiriClientController.php
+++ src/Http/Controllers/SiriClientController.php
@@ -3,11 +3,18 @@
 namespace TromsFylkestrafikk\Siri\Http\Controllers;
 
 use TromsFylkestrafikk\Siri\Models\SiriSubscription;
+use TromsFylkestrafikk\Xml\ChristmasTreeParser;
 
 class SiriClientController extends Controller
 {
     public function consume($channel, SiriSubscription $subscription)
     {
+        $reader = new ChristmasTreeParser();
+        $reader->addCallback(['Siri', 'SubscriptionResponse'], [$this, ]);
         return sprintf("Got request of type %s on subscription '%s'", $channel, $subscription->id);
+    }
+
+    public function subscriptionResponse(ChristmasTreeParser $reader)
+    {
     }
 }
