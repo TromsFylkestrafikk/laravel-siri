f74ec4e2f88a1d1f2f996b19f09253793b09df16
Kåre Slettnes <kaare@slettnes.org>
2022-01-10 10:33:02 +0100

    Issue #8: XML saved and fed to ChristmasTreeParser

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use TromsFylkestrafikk\Siri\Http\Controllers\SiriClientController;
 
-Route::post('consume/{channel}/{id}', [SiriClientController::class, 'consume'])->name('siri.consume');
+Route::post('consume/{channel}/{id}', [SiriClientController::class, 'consume'])
+    ->name('siri.consume')
+    ->where('channel', '(VM|ET|SX)');
--- src/Http/Controllers/SiriClientController.php
+++ src/Http/Controllers/SiriClientController.php
@@ -2,19 +2,80 @@
 
 namespace TromsFylkestrafikk\Siri\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use TromsFylkestrafikk\Siri\Models\SiriSubscription;
 use TromsFylkestrafikk\Xml\ChristmasTreeParser;
 
 class SiriClientController extends Controller
 {
-    public function consume($channel, SiriSubscription $subscription)
+    /**
+     * SIRI channel for our request.
+     *
+     * @var string
+     */
+    protected $channel;
+
+    /**
+     * Generated filename for incoming SIRI XML.
+     *
+     * @var string
+     */
+    protected $filename;
+
+    public function consume(Request $request, $channel, SiriSubscription $subscription)
     {
+        $this->channel = $channel;
+        $this->saveXml($request);
         $reader = new ChristmasTreeParser();
-        $reader->addCallback(['Siri', 'SubscriptionResponse'], [$this, ]);
+        $reader->open(Storage::disk(config('siri.disk'))->path($this->filename));
+        $reader->addCallback(['Siri', 'SubscriptionResponse'], [$this, 'subscriptionResponse'])
+            ->addCallback(['Siri', 'HeartbeatNotification'], [$this, 'heartbeatNotification'])
+            ->addCallback(['Siri', 'ServiceDelivery'], [$this, 'serviceDelivery'])
+            ->parse()
+            ->close();
         return sprintf("Got request of type %s on subscription '%s'", $channel, $subscription->id);
+    }
+
+    protected function saveXml(Request $request)
+    {
+        $this->filename = $this->makeXmlFilename();
+        $incoming = $request->getContent(true);
+        Storage::disk(config('siri.disk'))->put($this->filename, $incoming);
+    }
+
+    /**
+     * Generate a unique filename on our disk for incoming XML.
+     *
+     * @return string
+     */
+    protected function makeXmlFilename()
+    {
+        $timestamp = date('Y-m-d\TH:i:s');
+        $disk = Storage::disk(config('siri.disk'));
+        $folder = config('siri.folder');
+        $filename = sprintf("%s/siri-%s-%s.xml", $folder, strtolower($this->channel), $timestamp);
+
+        $counter = 0;
+        while ($disk->exists($filename)) {
+            $filename = sprintf("%s/%s-%s_%02d.xml", $folder, strtolower($this->channel), $timestamp, ++$counter);
+        }
+        return $filename;
     }
 
     public function subscriptionResponse(ChristmasTreeParser $reader)
     {
+        Log::debug("Got subscription response");
+    }
+
+    public function heartbeatNotification(ChristmasTreeParser $reader)
+    {
+        Log::debug("Got heartbeat notification response");
+    }
+
+    public function serviceDelivery(ChristmasTreeParser $reader)
+    {
+        Log::debug("Got service delivery. Actual content.");
     }
 }
