35079ac6ae0ae0e98b651b7467e15226966a9ff0
Kåre Slettnes <kaare@slettnes.org>
2022-01-13 11:47:41 +0100

    Issue #9: Added un-tested tree-mapped harvesting of xml to array

--- src/ServiceDelivery/VehicleMonitoringDelivery.php
+++ src/ServiceDelivery/VehicleMonitoringDelivery.php
@@ -3,6 +3,7 @@
 namespace TromsFylkestrafikk\Siri\ServiceDelivery;
 
 use Illuminate\Support\Facades\Log;
+use SimpleXMLElement;
 use TromsFylkestrafikk\Xml\ChristmasTreeParser;
 use TromsFylkestrafikk\Siri\Siri;
 
@@ -48,9 +49,56 @@ class VehicleMonitoringDelivery extends Base
         '//siri:MonitoredVehicleJourney/siri:MonitoredCall/siri:VehicleAtStop' => 'callVehicleAtStop',
     ];
 
+    public static $activityTree = [
+        'RecordedAtTime' => 'single',
+        'ProgressBetweenStops' => [
+            'LinkDistance' => 'single',
+            'Percentage' => 'single',
+        ],
+        'MonitoredVehicleJourney' => [
+            'LineRef' => 'single',
+            'FramedVehicleJourneyRef' => [
+                'DataFrameRef' => 'single',
+                'DatedVehicleJourneyRef' => 'single',
+            ],
+            'PublishedLineName' => 'single',
+            'Monitored' => 'single',
+            'VehicleLocation' => [
+                'Latitude' => 'single',
+                'Longitude' => 'single',
+            ],
+            'Bearing' => 'single',
+            'Delay' => 'single',
+            'VehicleRef' => 'single',
+            'PreviousCalls' => [
+                'PreviousCall' => [
+                    '#multiple' => true,
+                    'StopPointRef' => 'single',
+                    'VisitNumber' => 'single',
+                    'StopPointName' => 'single',
+                    'VehicleAtStop' => 'single',
+                ],
+            ],
+            'MonitoredCall' => [
+                'StopPointRef' => 'single',
+                'VisitNumber' => 'single',
+                'StopPointName' => 'single',
+                'VehicleAtStop' => 'single',
+            ],
+        ],
+    ];
+
+    public static $xpathPrevCallMap = [
+        'siri:StopPointRef' => 'stopPointRef',
+        'siri:VisitNumber' => 'visitNumber',
+        'siri:StopPointName' => 'stopPointName',
+        'siri:VehicleAtStop' => 'vehicleAtStop',
+    ];
+
     public function process()
     {
         parent::process();
+        dump($this->activities);
     }
 
     public function setupHandlers()
@@ -89,5 +137,63 @@ class VehicleMonitoringDelivery extends Base
             }
         }
         $this->activities[] = $activity;
+        $reader->halt();
+    }
+
+    public function getXmlElement($element, $map, SimpleXMLElement $xml)
+    {
+        $xml->registerXPathNamespace('siri', Siri::NS);
+        $elXml = $xml->xpath("siri:$element");
+        if (!count($elXml)) {
+            return null;
+        }
+        if (!is_array($map[$element])) {
+            return trim((string) $elXml[0]);
+        }
+
+        if (!empty($map[$element]['#multiple'])) {
+            $childItems = [];
+            foreach ($elXml as $childXml) {
+                $childItems[] = $this->getXmlChildElements($map[$element], $childXml);
+            }
+            return $childItems;
+        }
+        return $this->getXmlChildElements($map[$element], $elXml[0]);
+    }
+
+    protected function getXmlChildElements($map, SimpleXMLElement $xml)
+    {
+        $ret = [];
+        foreach ($map as $element => $children) {
+            $ret[$element] = $this->getXmlElement($element, $map, $xml);
+        }
+        return $ret;
+    }
+
+    protected function parsePreviousCalls(SimpleXMLElement $xml, &$activity)
+    {
+        // $actXml->registerXPathNamespace('siri', Siri::NS);
+        $prevCalls = [];
+        foreach ($xml->xpath('//siri:MonitoredVehicleJourney/siri:PreviousCalls/siri:PreviousCall') as $prev) {
+            $call = [];
+            $this->xmlToArray($prev, self::$xpathPrevCallMap, $call);
+            $prevCalls[] = $call;
+        }
+    }
+
+    /**
+     * @param SimpleXMLElement $xml
+     * @param array $map
+     * @param array $dest
+     */
+    protected function xmlToArray($xml, $map, &$dest)
+    {
+        $xml->registerXPathNamespace('siri', Siri::NS);
+        foreach ($map as $xpath => $destKey) {
+            $hits = $xml->xpath($xpath);
+            if (count($hits)) {
+                $dest[$destKey] = trim((string) $hits[0]);
+            }
+        }
     }
 }
