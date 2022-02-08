9ee6b95bcd672ef2698d5531e710dae7f9ab0078
Kåre Slettnes <kaare.slettnes@tffk.no>
2022-02-08 19:47:58 +0100

    Issue #24 ChangeBeforeUpdates must be a iso 8601 duration

--- src/Subscription/EtRequest.php
+++ src/Subscription/EtRequest.php
@@ -10,7 +10,7 @@ class EtRequest extends RequestBase implements SiriRequestContract
     public function getViewData()
     {
         return array_merge(parent::getViewData(), [
-            'change_before_updates' => 20,
+            'change_before_updates' => "PT30S",
         ]);
     }
 }
