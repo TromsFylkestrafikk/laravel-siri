0ff77b229606e265aab838afe24551e109575bdc
Kåre Slettnes <kaare@slettnes.org>
2022-11-18 14:43:54 +0100

    Issue #31: Added query scopes and index resource controller callback

--- src/Http/Controllers/PtSituationController.php
+++ src/Http/Controllers/PtSituationController.php
@@ -8,9 +8,21 @@ use TromsFylkestrafikk\Siri\Models\Sx\PtSituation;
 class PtSituationController extends Controller
 {
     /**
+     * Get a list of all open, valid SIRI SX Situations.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|\TromsFylkestrafikk\Siri\Models\Sx\PtSituation[]
+     */
+    public function index()
+    {
+        // @phpstan-ignore-next-line
+        return PtSituation::with(['affectedJourneys', 'affectedLines', 'affectedStopPoints'])->get();
+    }
+
+    /**
      * Display the specified resource.
      *
      * @param  PtSituation  $ptSituation
+     *
      * @return PtSituation
      */
     public function show(PtSituation $ptSituation)
