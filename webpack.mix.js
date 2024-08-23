eccd4f2f5d7648ac922b3cf4e54052efd7483703
Jørgen Belsaas <jorgen.belsaas@tromsfylke.no>
2024-08-23 12:30:26 +0200

    hack: build siri-emulation in current repo, hack to enable local subscription

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ const mix = require('laravel-mix');
  * Compile assets.
  */
 
-mix.js('vendor/tromsfylkestrafikk/laravel-siri/resources/js/app.js', 'public/siri/js')
+mix.js('resources/js/app.js', 'public/siri/js')
     .vue()
     .extract([
         'axios',
